4029093c68fb74c49b916263de28dd3ca7c57fcd
fauziahmilda <fauziah.milda@gmail.com>
2022-10-30 15:50:25 +0700

    first commit

---
+++ lib/pages/home/main_jasa.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/src/foundation/key.dart';
+import 'package:flutter/src/widgets/framework.dart';
+
+class MainJasa extends StatefulWidget {
+  const MainJasa({Key? key}) : super(key: key);
+
+  @override
+  State<MainJasa> createState() => _MainJasaState();
+}
+
+class _MainJasaState extends State<MainJasa> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
---
+++ lib/utils/colors.dart
@@ -0,0 +1,27 @@
+import 'dart:ui';
+import 'package:flutter/material.dart';
+
+class AppColors {
+  //text
+  static final Color textColor1 = const Color(0xFF29313d);
+  static final Color textColor2 = const Color(0xFFffffff);
+  //main
+  static final Color mainColor = const Color(0xFF48566A);
+  //icon
+  static final Color iconbgColor1 = const Color(0xFFCBAF87);
+  static final Color iconbgColor2 = const Color(0xFF95A2B7);
+  static final Color iconbgColor3 = const Color(0xFF5D6E89);
+  static final Color iconbgColor4 = const Color(0xFF7E6A56);
+  //button
+  static final Color buttonColor1 = const Color(0xFF48566A);
+  static final Color buttonColor2 = const Color(0xFF7E6A56);
+  static final Color buttonColor3 = const Color(0xFF29313D);
+  static final Color buttonColor4 = const Color(0xFF584A3C);
+  //background
+  static final Color bgColor1 = const Color(0xFFD9D9D9);
+  static final Color bgColor2 = const Color(0xFF48566A);
+  static final Color bgColor3 = const Color(0xFFCBAF87);
+  //etc
+  static final Color titleColor = const Color(0xFF7E6A56);
+  static final Color appbarColor = const Color(0xFF29313D);
+}
