54f1723116d540da7f93d1572342ca0d5adc5f74
fauziahmilda <fauziah.milda@gmail.com>
2022-11-07 01:15:00 +0700

    detailGuru page update1

--- lib/app/modules/detailGuru/views/detail_guru_view.dart
+++ lib/app/modules/detailGuru/views/detail_guru_view.dart
@@ -10,8 +10,48 @@ class DetailGuruView extends GetView<DetailGuruController> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: const Text('DetailGuruView'),
-        centerTitle: true,
+        title: Stack(
+          children: [Image.asset("assets/images/t1.png")],
+        ),
+      ),
+      bottomNavigationBar: Container(
+        height: 56,
+        margin: EdgeInsets.symmetric(vertical: 24, horizontal: 12),
+        child: Row(
+          children: <Widget>[
+            Container(
+              width: 66,
+              color: Colors.green,
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: <Widget>[
+                  Icon(Icons.chat, color: Colors.white),
+                  Text("CHAT", style: TextStyle(color: Colors.white))
+                ],
+              ),
+            ),
+            Container(
+              width: 66,
+              color: Colors.green,
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: <Widget>[
+                  Icon(Icons.notifications_active, color: Colors.white),
+                  Text("NOTIF", style: TextStyle(color: Colors.white))
+                ],
+              ),
+            ),
+            Expanded(
+              child: Container(
+                alignment: Alignment.center,
+                color: Colors.red,
+                child: Text("BUY NOW",
+                    style:
+                        TextStyle(fontWeight: FontWeight.bold, fontSize: 18)),
+              ),
+            ),
+          ],
+        ),
       ),
       body: Center(
         child: Text(
