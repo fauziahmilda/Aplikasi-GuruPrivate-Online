030d8c73b2c86a50b60d7b23bcfeded824bff4e6
fauziahmilda <fauziah.milda@gmail.com>
2022-11-09 20:16:57 +0700

    information page update

--- lib/app/modules/information/views/information_view.dart
+++ lib/app/modules/information/views/information_view.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 
 import 'package:get/get.dart';
 
+import '../../../routes/app_pages.dart';
 import '../controllers/information_controller.dart';
 
 class InformationView extends GetView<InformationController> {
@@ -10,8 +11,16 @@ class InformationView extends GetView<InformationController> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: const Text('InformationView'),
+        title: const Text('Information'),
         centerTitle: true,
+        elevation: 0,
+        backgroundColor: Color(0xFF584A3C),
+        leading: IconButton(
+          onPressed: () => Get.offAllNamed(Routes.SETTING),
+          icon: Icon(Icons.arrow_back),
+        ),
+        shape: RoundedRectangleBorder(
+            borderRadius: BorderRadius.vertical(bottom: Radius.circular(30))),
       ),
       body: Center(
         child: Text(
