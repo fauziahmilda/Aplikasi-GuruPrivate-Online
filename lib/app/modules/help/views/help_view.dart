4d0035fad08b6e7c77791bdea26eb69e9d5fd12e
fauziahmilda <fauziah.milda@gmail.com>
2022-11-09 20:17:57 +0700

    help page update

--- lib/app/modules/help/views/help_view.dart
+++ lib/app/modules/help/views/help_view.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 
 import 'package:get/get.dart';
 
+import '../../../routes/app_pages.dart';
 import '../controllers/help_controller.dart';
 
 class HelpView extends GetView<HelpController> {
@@ -10,8 +11,16 @@ class HelpView extends GetView<HelpController> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: const Text('HelpView'),
+        title: const Text('Help and Support'),
         centerTitle: true,
+        elevation: 0,
+        backgroundColor: Color(0xFF584A3C),
+        leading: IconButton(
+          onPressed: () => Get.offAllNamed(Routes.SETTING),
+          icon: Icon(Icons.arrow_back),
+        ),
+        shape: RoundedRectangleBorder(
+            borderRadius: BorderRadius.vertical(bottom: Radius.circular(30))),
       ),
       body: Center(
         child: Text(
